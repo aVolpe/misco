490099a092878ae7a60668d1d7a286189c6baddf
Arturo Volpe <arturovolpe@gmail.com>
2021-02-09 18:39:06 -0300

    feat: Add base backend
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

---
+++ server/src/main/java/py/com/volpe/misco/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package py.com.volpe.misco.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/py/com/volpe/misco/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package py.com.volpe.misco.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
