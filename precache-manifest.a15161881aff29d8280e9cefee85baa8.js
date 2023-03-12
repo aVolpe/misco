f98e70e14ffbde81954fc41ff343df4baa18a7ab
aVolpe <arturovolpe@gmail.com>
2023-03-12 04:25:08 +0000

    Deploying to gh-pages from @ aVolpe/misco@c14fbc76ed22035781ae1f5b1ea91394e0135313 🚀

---
+++ precache-manifest.a15161881aff29d8280e9cefee85baa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f9b26fb536c4538ff83ff7499860b7a",
+    "url": "/misco/index.html"
+  },
+  {
+    "revision": "c35741a82824f35cfcd3",
+    "url": "/misco/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "6d301d1867222c969eb1",
+    "url": "/misco/static/css/main.ffa29014.chunk.css"
+  },
+  {
+    "revision": "c35741a82824f35cfcd3",
+    "url": "/misco/static/js/2.7303f767.chunk.js"
+  },
+  {
+    "revision": "374dcffa024aa2ae1f3fa11693e72ecb",
+    "url": "/misco/static/js/2.7303f767.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d301d1867222c969eb1",
+    "url": "/misco/static/js/main.bc024e18.chunk.js"
+  },
+  {
+    "revision": "916065bcfd1f304fc251",
+    "url": "/misco/static/js/runtime-main.320ca70f.js"
+  }
+]);
