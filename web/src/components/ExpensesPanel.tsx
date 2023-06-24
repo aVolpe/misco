bc33439bdeb3b177ff0d90e0367f2fdb3a331690
Arturo Volpe <arturovolpe@gmail.com>
2023-06-24 19:50:09 -0400

    feat: Move expense table to separate component
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

---
+++ web/src/components/ExpensesPanel.tsx
@@ -0,0 +1,180 @@
+import {Expense} from '../set/Model';
+import React, {useEffect, useMemo, useState} from 'react';
+import {useDebounce} from '../utils/Hooks';
+import dayjs from 'dayjs';
+import {SETListManipulatorService} from '../set/SETListManipulatorService';
+import {Button, Col, DatePicker, Input, Row, Space, Table, Typography} from 'antd';
+import {TableRowSelection} from 'antd/es/table/interface';
+import {ExpenseDocumentType, PaymentType} from '../set/V2Enums';
+import {formatMoney} from '../utils/formatters';
+import {sumBy} from 'lodash';
+import {MiscoTag} from '../tags/MiscoTag';
+
+export function ExpensePanel(props: {
+    period: number;
+    data: Expense[];
+    doRemove: (expenseId: number) => void;
+    hideActions?: boolean;
+    doEdit: (expense: Expense) => void;
+    onSelectionChange?: (rows: Expense[]) => void;
+}) {
+
+    const [query, setQuery] = useState('');
+    const debouncedQuery = useDebounce(query, 500);
+    const [date, setDate] = useState<[dayjs.Dayjs, dayjs.Dayjs]>([
+        dayjs().year(props.period).startOf('year').startOf('day'),
+        dayjs().year(props.period).endOf('year').endOf('day'),
+    ]);
+    const [data, setData] = useState<Expense[]>(props.data);
+
+    useEffect(() => {
+        setData(new SETListManipulatorService().filterExpenses(props.data, debouncedQuery, date[0], date[1]))
+    }, [debouncedQuery, props.data, date]);
+
+    const selection: TableRowSelection<Expense> | undefined = useMemo(() => {
+        if (!props.onSelectionChange) return undefined;
+        return {
+            type: 'checkbox',
+            onChange: (selectedRowKeys: React.Key[], selectedRows: Expense[]) => {
+                console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+                props.onSelectionChange!(selectedRows);
+            },
+        };
+    }, [props.onSelectionChange]);
+
+    return <>
+        <Row gutter={[8, 8]} align="middle">
+            <Col span={2} style={{textAlign: 'right', fontWeight: 'bold'}} offset={1}>
+                Búsqueda:
+            </Col>
+            <Col span={9}>
+                <Input placeholder="Por ruc/nombre/nro factura"
+                       value={query}
+                       onChange={t => setQuery(t.target.value)}/>
+            </Col>
+            <Col span={2} style={{textAlign: 'right', fontWeight: 'bold'}}>
+                Rango:
+            </Col>
+            <Col span={9}>
+                <DatePicker.RangePicker value={date}
+                                        style={{width: '100%'}}
+                                        allowClear={false}
+                                        ranges={{
+                                            [new Date().getFullYear() - 2]: [
+                                                dayjs().subtract(2, 'year').startOf('year'),
+                                                dayjs().subtract(2, 'year').endOf('year'),
+                                            ],
+                                            [new Date().getFullYear() - 1]: [
+                                                dayjs().subtract(1, 'year').startOf('year'),
+                                                dayjs().subtract(1, 'year').endOf('year'),
+                                            ],
+                                            [new Date().getFullYear()]: [
+                                                dayjs().startOf('year'),
+                                                dayjs().endOf('year')
+                                            ]
+                                        }}
+                                        onChange={values => {
+                                            if (!values) return;
+                                            setDate([
+                                                values[0]!.startOf('day'),
+                                                values[1]!.endOf('day')
+                                            ]);
+                                        }}
+                />
+            </Col>
+        </Row>
+        <Row style={{paddingTop: 5}}>
+            <ExpenseTable invoices={data}
+                          selection={selection}
+                          hideActions={props.hideActions}
+                          onRemove={r => props.doRemove(r.id)}
+                          onEdit={props.doEdit}/>
+        </Row>
+    </>
+}
+
+function ExpenseTable(props: {
+    invoices: Expense[]
+    onEdit(row: Expense): void;
+    onRemove(row: Expense): void;
+    selection?: TableRowSelection<Expense>;
+    hideActions?: boolean;
+}) {
+    return <Table<Expense>
+        dataSource={props.invoices}
+        size="small"
+        rowSelection={props.selection}
+        pagination={{
+            pageSize: 10,
+            showTotal: (t, r) => `Mostrando ${r[0]} a ${r[1]} de ${t} registros.`
+        }}
+        rowKey="id"
+        style={{width: '100%'}}
+        columns={[{
+            title: 'ID',
+            dataIndex: 'id',
+            defaultSortOrder: 'descend',
+            sorter: (a, b) => a.id - b.id,
+            render: (_, row) => <Space direction="vertical">
+                {row.id}
+                <MiscoTag tags={row.tags}/>
+            </Space>
+        }, {
+            title: 'Tipo',
+            dataIndex: 'type',
+            align: 'right',
+            render: (_, row) => <>
+                {ExpenseDocumentType[row.type]}
+                {row.paymentType && <><br/> <small>{PaymentType[row.paymentType]}</small></>}
+            </>,
+            sorter: (a, b) => `${ExpenseDocumentType[a.type]}`
+                .localeCompare(`${ExpenseDocumentType[b.type]}`),
+        }, {
+            title: 'Fecha',
+            dataIndex: 'date',
+            sorter: (a, b) => a.date.localeCompare(b.date),
+        }, {
+            title: 'Emisor',
+            dataIndex: 'identifier',
+            align: 'left',
+            sorter: (a, b) => a.name.localeCompare(b.name),
+            render: (_, row) => <>
+                {row.name} ({row.identifier})
+            </>
+        }, {
+            title: 'Factura',
+            dataIndex: 'voucher',
+            align: 'right',
+            render: (_, row) => <>
+                {row.voucher}
+                <br/><small>{row.letterhead}</small>
+            </>
+        }, {
+            title: 'Monto',
+            align: 'right',
+            dataIndex: 'amount',
+            render: (a: number) => formatMoney(a),
+            sorter: (a, b) => a.amount - b.amount,
+        }, props.hideActions === true ? {} : {
+            title: 'Acciones', dataIndex: '', render: (_, row) => {
+                return <>
+                    <Button onClick={() => props.onEdit(row)}>Editar</Button>
+                    <Button danger onClick={() => props.onRemove(row)}>Eliminar</Button>
+                </>
+            }
+        }]}
+
+        summary={() => {
+            const sum = sumBy(props.invoices, 'amount');
+            return <tr>
+                <th colSpan={3}>Total (todas las filas)</th>
+                <td colSpan={2}/>
+                <td className="ant-table-cell" style={{textAlign: 'right', padding: 8}}>
+                    <Typography.Text>{formatMoney(sum)}</Typography.Text>
+                </td>
+                <td/>
+            </tr>
+        }}
+    />
+
+}
--- web/src/pages/ExpenseListPage.tsx
+++ web/src/pages/ExpenseListPage.tsx
@@ -1,19 +1,14 @@
 import React, {useCallback, useEffect, useMemo, useState} from 'react';
-import {Button, Col, DatePicker, Input, message, Row, Table, Typography} from 'antd';
+import {Col, message, Row} from 'antd';
 import {Async, NRHelper, NRWrapper} from '../Model';
 import {Help} from '../components/Help';
 import {Person} from '../RucAPI';
 import {ExpenseForm, ExpenseFormData} from '../components/ExpenseForm';
-import {formatMoney} from '../utils/formatters';
 import {GlobalHotKeys} from 'react-hotkeys';
 import {PersonWithLetterhead, SETService} from '../set/SETService';
-import {useDebounce} from '../utils/Hooks';
-import {SETListManipulatorService} from '../set/SETListManipulatorService';
-import {sumBy} from 'lodash';
 import {Expense} from "../set/Model";
 import {PersonType} from "../set/ParametroEgreso";
-import {ExpenseDocumentType, PaymentType} from "../set/V2Enums";
-import dayjs from 'dayjs';
+import {ExpensePanel} from '../components/ExpensesPanel';
 
 export const emptyOwner: Person = {old: '', div: '', name: '', doc: ''};
 
@@ -41,17 +36,6 @@ export function ExpenseListPage(props: {
     const service = useMemo(() => new SETService(props.period, props.owner, props.type, props.data),
         [props.data, props.period, props.type, props.owner]);
 
-    const [query, setQuery] = useState('');
-    const debouncedQuery = useDebounce(query, 500);
-    const [date, setDate] = useState<[dayjs.Dayjs, dayjs.Dayjs]>([
-        dayjs().year(props.period).startOf('year').startOf('day'),
-        dayjs().year(props.period).endOf('year').endOf('day'),
-    ]);
-    const [data, setData] = useState<Expense[]>(props.data);
-
-    useEffect(() => {
-        setData(new SETListManipulatorService().filterExpenses(props.data, debouncedQuery, date[0], date[1]))
-    }, [debouncedQuery, props.data, date]);
 
     function onNewExpense() {
         setCurrent(JSON.parse(JSON.stringify((defaultExpense))));
@@ -85,51 +69,11 @@ export function ExpenseListPage(props: {
         }}>
         <Row gutter={[8, 8]} style={{padding: 8}}>
             <Col span={18}>
-                <Row gutter={[8, 8]} align="middle">
-                    <Col span={2} style={{textAlign: 'right', fontWeight: 'bold'}} offset={1}>
-                        Búsqueda:
-                    </Col>
-                    <Col span={9}>
-                        <Input placeholder="Por ruc/nombre/nro factura"
-                               value={query}
-                               onChange={t => setQuery(t.target.value)}/>
-                    </Col>
-                    <Col span={2} style={{textAlign: 'right', fontWeight: 'bold'}}>
-                        Rango:
-                    </Col>
-                    <Col span={9}>
-                        <DatePicker.RangePicker value={date}
-                                                style={{width: '100%'}}
-                                                allowClear={false}
-                                                ranges={{
-                                                    [new Date().getFullYear() - 2]: [
-                                                        dayjs().subtract(2, 'year').startOf('year'),
-                                                        dayjs().subtract(2, 'year').endOf('year'),
-                                                    ],
-                                                    [new Date().getFullYear() - 1]: [
-                                                        dayjs().subtract(1, 'year').startOf('year'),
-                                                        dayjs().subtract(1, 'year').endOf('year'),
-                                                    ],
-                                                    [new Date().getFullYear()]: [
-                                                        dayjs().startOf('year'),
-                                                        dayjs().endOf('year')
-                                                    ]
-                                                }}
-                                                onChange={values => {
-                                                    if (!values) return;
-                                                    setDate([
-                                                        values[0]!.startOf('day'),
-                                                        values[1]!.endOf('day')
-                                                    ]);
-                                                }}
-                        />
-                    </Col>
-                </Row>
-                <Row style={{paddingTop: 5}}>
-                    <InvoiceTable invoices={data}
-                                  onRemove={r => props.doRemove(r.id)}
-                                  onEdit={onEdit}/>
-                </Row>
+                <ExpensePanel data={props.data}
+                              doEdit={onEdit}
+                              doRemove={props.doRemove}
+                              period={props.period}
+                />
             </Col>
             <Col span={6} style={{border: '1px solid rgb(235, 237, 240)'}}>
                 {current
@@ -144,6 +88,7 @@ export function ExpenseListPage(props: {
         </Row>
     </GlobalHotKeys>
 }
+
 
 function InvoiceEditor(props: {
     editType: 'NEW' | 'EDIT';
@@ -183,81 +128,3 @@ function InvoiceEditor(props: {
                         onNewRuc={onNewRuc}/>;
 }
 
-function InvoiceTable(props: {
-    invoices: Expense[]
-    onEdit(row: Expense): void;
-    onRemove(row: Expense): void;
-}) {
-    return <Table<Expense>
-        dataSource={props.invoices}
-        size="small"
-        pagination={{
-            pageSize: 10,
-            showTotal: (t, r) => `Mostrando ${r[0]} a ${r[1]} de ${t} registros.`
-        }}
-        rowKey="id"
-        style={{width: '100%'}}
-        columns={[{
-            title: 'ID',
-            dataIndex: 'id',
-            defaultSortOrder: 'descend',
-            sorter: (a, b) => a.id - b.id,
-        }, {
-            title: 'Tipo',
-            dataIndex: 'type',
-            align: 'right',
-            render: (_, row) => <>
-                {ExpenseDocumentType[row.type]}
-                {row.paymentType && <><br/> <small>{PaymentType[row.paymentType]}</small></>}
-            </>,
-            sorter: (a, b) => `${ExpenseDocumentType[a.type]}`
-                .localeCompare(`${ExpenseDocumentType[b.type]}`),
-        }, {
-            title: 'Fecha',
-            dataIndex: 'date',
-            sorter: (a, b) => a.date.localeCompare(b.date),
-        }, {
-            title: 'Emisor',
-            dataIndex: 'identifier',
-            align: 'left',
-            sorter: (a, b) => a.name.localeCompare(b.name),
-            render: (_, row) => <>
-                {row.name} ({row.identifier})
-            </>
-        }, {
-            title: 'Factura',
-            dataIndex: 'voucher',
-            align: 'right',
-            render: (_, row) => <>
-                {row.voucher}
-                <br/><small>{row.letterhead}</small>
-            </>
-        }, {
-            title: 'Monto',
-            align: 'right',
-            dataIndex: 'amount',
-            render: (a: number) => formatMoney(a),
-            sorter: (a, b) => a.amount - b.amount,
-        }, {
-            title: 'Acciones', dataIndex: '', render: (_, row) => {
-                return <>
-                    <Button onClick={() => props.onEdit(row)}>Editar</Button>
-                    <Button danger onClick={() => props.onRemove(row)}>Eliminar</Button>
-                </>
-            }
-        }]}
-
-        summary={() => {
-            const sum = sumBy(props.invoices, 'amount');
-            return <tr>
-                <th colSpan={3}>Total (todas las filas)</th>
-                <td colSpan={2}/>
-                <td className="ant-table-cell" style={{textAlign: 'right', padding: 8}}>
-                    <Typography.Text>{formatMoney(sum)}</Typography.Text>
-                </td>
-                <td/>
-            </tr>
-        }}
-    />
-
-}
