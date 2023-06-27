8f9564b797c55bbbe506194dd2bb6e27f52f213f
Arturo Volpe <arturovolpe@gmail.com>
2023-06-27 00:31:47 -0400

    feat: Add component to search by tags
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

--- web/src/tags/MiscoTag.tsx
+++ web/src/tags/MiscoTag.tsx
@@ -1,5 +1,52 @@
-import {Tag} from 'antd';
+import {Tag, theme} from 'antd';
+import {AS_OPTIONS, TAGS_LOOKUP} from './Model';
+import React, {useCallback, useEffect, useState} from 'react';
+import {PlusOutlined} from '@ant-design/icons';
 
 export function MiscoTag(props: { tags?: string[] }) {
-    return (props.tags ?? []).map(t => <Tag>{t}</Tag>)
+    return (props.tags ?? []).map(t => <Tag key={t}>{TAGS_LOOKUP[t] || t}</Tag>)
+}
+
+export function TagBar(props: {
+    onChanged: (tags: string[]) => void,
+}) {
+
+    const {token} = theme.useToken();
+    const tagPlusStyle = {
+        background: token.colorBgContainer,
+        borderStyle: 'dashed',
+    };
+    const [tags, setTags] = useState<string[]>([]);
+
+    const toggleTag = useCallback((e: React.MouseEvent<HTMLSpanElement>) => {
+        const tag = e.currentTarget.id;
+
+        setTags(pre => {
+            const wasSelected = pre.includes(tag);
+            const wasNegated = pre.includes(`!${tag}`);
+            if (wasSelected) return pre.map(pt => pt === tag ? `!${tag}` : pt);
+            if (wasNegated) return pre.filter(pt => pt !== `!${tag}`);
+            return [...pre, tag];
+        });
+    }, []);
+
+    useEffect(() => props.onChanged(tags), [tags]);
+
+    return <>
+        {AS_OPTIONS.map(o => {
+            const selected = tags.includes(o.value);
+            const negated = tags.includes(`!${o.value}`);
+            const noUsed = !selected && !negated;
+            const style = noUsed ? tagPlusStyle : {};
+            const color = selected ? 'green' : negated ? 'red' : undefined;
+            return <Tag key={o.value}
+                        id={o.value}
+                        color={color}
+                        style={style} onClick={toggleTag}>
+                {noUsed && <PlusOutlined/>}
+                {negated && '!'}
+                {o.label}
+            </Tag>
+        })}
+    </>
 }
--- web/src/tags/Model.ts
+++ web/src/tags/Model.ts
@@ -1,16 +1,31 @@
-import {SelectProps} from 'antd';
-
 export const ALL_TAGS: Array<MiscoTag> = [{
     id: 'ELECTRONIC_INVOICE',
     label: 'Factura electronica'
+}, {
+    id: 'REJECTED',
+    label: "Rechazado en Marangatu"
+}, {
+    id: 'MANUALLY_REGISTERED',
+    label: "Registrado manualmente en marangatu"
+}, {
+    id: 'ORIGINAL_NOT_FOUND',
+    label: "No se encuentra el original"
+}, {
+    id: 'FIXED_PENDING_UPLOAD',
+    label: "Corregido localmente"
 }];
+
+export const TAGS_LOOKUP: Record<string, string> = ALL_TAGS.reduce((acc, t) => ({
+    ...acc,
+    [t.id]: t.label
+}), {});
 
 export interface MiscoTag {
     id: string;
     label: string;
 }
 
-export const AS_OPTIONS: SelectProps['options'] = ALL_TAGS.map(t => ({
+export const AS_OPTIONS: Array<{ label: string, value: string }> = ALL_TAGS.map(t => ({
     label: t.label,
     value: t.id
 }));
