a6a15d969c502d330a7fc5705c52da6c27cd5ba2
Arturo Volpe <arturovolpe@gmail.com>
2023-09-12 22:09:48 -0400

    feat: Remove backend
    Signed-off-by: Arturo Volpe <arturovolpe@gmail.com>

--- server/src/main/java/py/com/volpe/misco/backend/ReportController.java
+++
@@ -1,8 +0,0 @@
-package py.com.volpe.misco.backend;/**
- * 
- *
- * @author Arturo Volpe
- * @since 2021-02-09
- */
-public class ReportController {
-}
--- server/src/main/java/py/com/volpe/misco/backend/report/V2021ReportManager.java
+++
@@ -1,8 +0,0 @@
-package py.com.volpe.misco.backend.report;/**
- * 
- *
- * @author Arturo Volpe
- * @since 2021-02-10
- */
-public class V2021ReportManager {
-}
